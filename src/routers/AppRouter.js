f50c4deea5b595a745c451b042044f4202bc7c12
JuanCarlosCaychoPaucar <juan.carlos.caycho.paucar@outlook.com.pe>
2021-04-20 14:13:43 -0500

    Font agregada al proyecto

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { HomeScreen } from '../screens/HomeScreen';
 
 export const AppRouter = () => {
     return (
-        <HashRouter>
+        <Router>
             <Switch>
                 <Route path={"/"} component={HomeScreen} />
             </Switch>
-        </HashRouter>
+        </Router>
     )
 }
